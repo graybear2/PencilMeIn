8c04b8fa8580cc38b34c11ff9ebb0e3fc3704b4d
Kevin Brill <kevinjbrill@utexas.edu>
2018-03-23 13:11:15 -0500

    servlet code to add friend

---
+++ PencilMeIn/src/main/java/pencilmein/FriendServlet.java
@@ -0,0 +1,46 @@
+package pencilmein;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.googlecode.objectify.ObjectifyService;
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FriendServlet extends HttpServlet {
+    
+    public FriendServlet() {
+        
+    }
+    
+    static {
+        ObjectifyService.register(Student.class);
+    }
+    
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();  
+        
+        //Redirect user if they're not logged in
+        if(user == null) {
+            try {
+                resp.sendRedirect("/index.jsp");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+        Student student = ofy().load().type(Student.class).id(user.getEmail()).now();
+        String friend_email = req.getParameter("email");
+        Student friend = ofy().load().type(Student.class).id(friend_email).now();
+        student.addFriend(friend);
+        
+         
+    }
+}
